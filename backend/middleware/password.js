f200dccc3be20075037779f75a9c0cd28499430f
DimPach <mrdimpac@hotmail.fr>
2021-06-17 15:55:04 +0200

    ajustement

--- backend/middleware/password.js
+++ backend/middleware/password.js
@@ -3,7 +3,7 @@ const passwordValidatorSchema = require('../models/password.js');
 module.exports = (req, res, next)=> { 
     try {
         if(!passwordValidatorSchema.validate(req.body.password)) {
-            console.log("if")
+            console.log(passwordValidatorSchema.validate(req.body.password, { list: true }))
             res.status(400).json({error: "Mauvais mot de passe"})
         } else {
             console.log("else")
